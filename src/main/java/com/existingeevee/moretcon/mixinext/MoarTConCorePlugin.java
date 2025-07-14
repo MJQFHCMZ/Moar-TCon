a4f3d8b578a06a626b35afe01b88f93f7f4135ec
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2025-07-14 03:00:00 -0500

    added fermium softdep

--- src/main/java/com/existingeevee/moretcon/mixinext/MoarTConCorePlugin.java
+++ src/main/java/com/existingeevee/moretcon/mixinext/MoarTConCorePlugin.java
@@ -13,42 +13,57 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 public class MoarTConCorePlugin implements IFMLLoadingPlugin {
 
-	public MoarTConCorePlugin() {
+	private static final String vanillaMixins = "mixins." + ModInfo.MODID + ".json"; // Things that change vanilla code
+	private static final String loaderMixins = "mixins." + ModInfo.MODID + ".loader.json"; // The one mixin that loads the late mixins
+	private static final String nonvanillaMixins = "mixins." + ModInfo.MODID + ".late.json"; // All of the modded stuff.
 
+	public MoarTConCorePlugin() {
 		System.out.println("Go my mixin...");
-		System.out.println("Cause havoc as you see fit...");
-		
+
+		try {
+			fermiumbooter.FermiumRegistryAPI.getRejectMixins(); //empty call to see if its there. we dont care about the return value
+
+			System.out.println("Take its hand, and achieve greatness...");
+
+			MixinBootstrap.init();
+			MixinExtrasBootstrap.init();
+
+			fermiumbooter.FermiumRegistryAPI.enqueueMixin(true, nonvanillaMixins);
+			fermiumbooter.FermiumRegistryAPI.enqueueMixin(false, vanillaMixins);
+			return;
+		} catch (NoClassDefFoundError  e) {
+		}
+
+		System.out.println("With no one left to guide you, cause havoc as you see fit...");
+
 		MixinBootstrap.init();
 		MixinExtrasBootstrap.init();
-		
-        Mixins.addConfiguration("mixins." + ModInfo.MODID + ".loader.json");
-        Mixins.addConfiguration("mixins." + ModInfo.MODID + ".json");
+
+		Mixins.addConfiguration(loaderMixins); // Welp ig we need to do it ourselves
+		Mixins.addConfiguration(vanillaMixins);
 	}
 
 	@Override
-	public String[] getASMTransformerClass()
-	{
+	public String[] getASMTransformerClass() {
 		return new String[0];
 	}
-	
+
 	@Override
-	public String getModContainerClass()
-	{
+	public String getModContainerClass() {
 		return null;
 	}
-	
+
 	@Override
-	public String getSetupClass()
-	{
+	public String getSetupClass() {
 		return null;
 	}
-	
+
 	@Override
-	public void injectData(Map<String, Object> data) { }
-	
+	public void injectData(Map<String, Object> data) {
+	}
+
 	@Override
-	public String getAccessTransformerClass()
-	{
+	public String getAccessTransformerClass() {
 		return null;
 	}
 }
