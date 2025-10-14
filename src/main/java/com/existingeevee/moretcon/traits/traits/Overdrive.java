4f9067c30e13c78a8405cfb7749bfad09ecb33e0
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2025-10-14 13:16:54 -0500

    ffs thanks overdrive

--- src/main/java/com/existingeevee/moretcon/traits/traits/Overdrive.java
+++ src/main/java/com/existingeevee/moretcon/traits/traits/Overdrive.java
@@ -1,10 +1,12 @@
 package com.existingeevee.moretcon.traits.traits;
 
 import com.existingeevee.moretcon.other.utils.MiscUtils;
+import com.existingeevee.moretcon.traits.ModTraits;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
+import slimeknights.tconstruct.library.utils.ToolHelper;
 
 public class Overdrive extends AbstractTrait {
 
@@ -14,7 +16,7 @@ public class Overdrive extends AbstractTrait {
 
 	@Override
 	public void afterHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damageDealt, boolean wasCritical, boolean wasHit) {
-		if (wasHit) {
+		if (wasHit && (ToolHelper.getTraits(tool).contains(ModTraits.polyshotProj) ? random.nextDouble() < 0.25 : true)) {
 			target.hurtResistantTime /= 2;
 		}
 	}
