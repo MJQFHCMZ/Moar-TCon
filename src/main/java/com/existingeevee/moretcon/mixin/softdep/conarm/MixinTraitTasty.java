47016e365c31921d7b0cd09304b96f3ee421d838
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2025-10-28 16:00:19 -0500

    saturpigting now works on armor

--- src/main/java/com/existingeevee/moretcon/mixin/softdep/conarm/MixinTraitTasty.java
+++ src/main/java/com/existingeevee/moretcon/mixin/softdep/conarm/MixinTraitTasty.java
@@ -1,4 +1,4 @@
-package com.existingeevee.moretcon.mixin.late.common;
+package com.existingeevee.moretcon.mixin.softdep.conarm;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -7,22 +7,22 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import com.existingeevee.moretcon.traits.ModTraits;
 import com.llamalad7.mixinextras.sugar.Local;
 
+import c4.conarm.common.armor.traits.TraitTasty;
 import net.minecraft.item.ItemStack;
-import slimeknights.tconstruct.tools.traits.TraitTasty;
 
 @Mixin(TraitTasty.class)
 public class MixinTraitTasty {
 
-	@ModifyConstant(method = "nom", constant = @Constant(intValue = 1), remap = false)
-	private int moretcon$ModifyConstant$nom$int1(int value, @Local ItemStack tool) {
+	@ModifyConstant(method = "eatToRepair", constant = @Constant(intValue = 1), remap = false)
+	private int moretcon$ModifyConstant$eatToRepair$int1(int value, @Local ItemStack tool) {
 		if (ModTraits.saturpigting.isToolWithTrait(tool)) {
 			value += 4;
 		}
 		return value;
 	}
 	
-	@ModifyConstant(method = "nom", constant = @Constant(floatValue = 0), remap = false)
-	private float moretcon$ModifyConstant$nom$float0(float value, @Local ItemStack tool) {
+	@ModifyConstant(method = "eatToRepair", constant = @Constant(floatValue = 0), remap = false)
+	private float moretcon$ModifyConstant$eatToRepair$float0(float value, @Local ItemStack tool) {
 		if (ModTraits.saturpigting.isToolWithTrait(tool)) {
 			value += 0.8;
 		}
