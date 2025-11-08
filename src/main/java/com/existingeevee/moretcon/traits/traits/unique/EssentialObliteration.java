85a3b814317c0968fa1d683258629212ae196fb9
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2025-11-07 18:12:40 -0600

    reworked e.e.

--- src/main/java/com/existingeevee/moretcon/traits/traits/unique/EssentialObliteration.java
+++ src/main/java/com/existingeevee/moretcon/traits/traits/unique/EssentialObliteration.java
@@ -3,29 +3,35 @@ package com.existingeevee.moretcon.traits.traits.unique;
 import java.util.UUID;
 
 import com.existingeevee.moretcon.other.utils.MiscUtils;
+import com.existingeevee.moretcon.other.utils.ReequipHack;
 import com.existingeevee.moretcon.traits.traits.abst.NumberTrackerTrait;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import slimeknights.tconstruct.gadgets.Exploder;
+import slimeknights.tconstruct.gadgets.entity.ExplosionEFLN;
 import slimeknights.tconstruct.library.utils.ToolHelper;
 
 public class EssentialObliteration extends NumberTrackerTrait {
 
 	public EssentialObliteration() {
 		super(MiscUtils.createNonConflictiveName("essential_obliteration"), 0);
+		ReequipHack.registerIgnoredKey(this.getModifierIdentifier());
 	}
 
 	@Override
 	public int getNumberMax(ItemStack stack) {
-		return 50;
+		return 60;
 	}
 
 	@Override
@@ -37,18 +43,33 @@ public class EssentialObliteration extends NumberTrackerTrait {
 	public void miningSpeed(ItemStack tool, PlayerEvent.BreakSpeed event) {
 		if (event.getEntityLiving().isSneaking()) {
 			event.setNewSpeed(event.getNewSpeed() * 0.0625f);
-		} else if (this.getNumber(tool) > 0) {
-			event.setNewSpeed(event.getNewSpeed() * 20f);
 		}
 	}
 
 	@Override
 	public void afterBlockBreak(ItemStack tool, World world, IBlockState state, BlockPos pos, EntityLivingBase player, boolean wasEffective) {
-		if (player.isSneaking()) {
-			int maxToAdd = this.getNumberMax(tool) - this.getNumber(tool);
-			this.addNumber(tool, Math.min(5, maxToAdd));
-		} else if (this.getNumber(tool) > 0) {
-			this.removeNumber(tool, 1);
+		if (player.isSneaking() && this.getNumber(tool) >= 10) {
+			if (player instanceof EntityPlayer && ((EntityPlayer) player).getCooldownTracker().hasCooldown(tool.getItem()))
+				return;
+			
+			if (!world.isRemote) {
+				float str = (float) Math.sqrt(2 * ToolHelper.getActualMiningSpeed(tool));
+				ExplosionEFLN explosion = new ExplosionEFLN(world, player, pos.getX(), pos.getY(), pos.getZ(), str, false, false);
+				if (!ForgeEventFactory.onExplosionStart(world, explosion)) {
+					Exploder.startExplosion(world, explosion, player, pos, str, str);
+					this.removeNumber(tool, 10);
+					if (player instanceof EntityPlayer)
+						((EntityPlayer) player).getCooldownTracker().setCooldown(tool.getItem(), 100);;
+
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onUpdate(ItemStack tool, World world, Entity entity, int itemSlot, boolean isSelected) {
+		if (!world.isRemote && world.getWorldTime() % 100 == 0) {
+			this.addNumber(tool, 1);
 		}
 	}
 
@@ -58,7 +79,7 @@ public class EssentialObliteration extends NumberTrackerTrait {
 	public void onLivingUpdate(LivingUpdateEvent e) {
 		if ((e.getEntityLiving() instanceof EntityPlayer)) {
 			ItemStack stack = e.getEntityLiving().getHeldItemMainhand();
-			if (this.isToolWithTrait(stack) && !e.getEntityLiving().isSneaking() && this.getNumber(stack) > 0 && !ToolHelper.isBroken(stack)) {
+			if (this.isToolWithTrait(stack) && e.getEntityLiving().isSneaking() && this.getNumber(stack) >= 10 && !ToolHelper.isBroken(stack)) {
 				if (!e.getEntityLiving().getAttributeMap().getAttributeInstance(EntityPlayer.REACH_DISTANCE).hasModifier(ESSENCORE_MINING_REACH)) {
 					e.getEntityLiving().getAttributeMap().getAttributeInstance(EntityPlayer.REACH_DISTANCE).applyModifier(ESSENCORE_MINING_REACH);
 				}
