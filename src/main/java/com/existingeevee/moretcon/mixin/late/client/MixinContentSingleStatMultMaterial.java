df97d6e3fab212c3cdfcae98f28dfba914b410c3
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2026-05-31 09:43:19 -0500

    Update MixinContentSingleStatMultMaterial.java

--- src/main/java/com/existingeevee/moretcon/mixin/late/client/MixinContentSingleStatMultMaterial.java
+++ src/main/java/com/existingeevee/moretcon/mixin/late/client/MixinContentSingleStatMultMaterial.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.existingeevee.moretcon.materials.UniqueMaterial;
+import com.existingeevee.moretcon.materials.IUniqueMaterial;
 import com.google.common.collect.Lists;
 
 import net.minecraft.item.ItemStack;
@@ -31,8 +31,8 @@ public class MixinContentSingleStatMultMaterial {
 
 	@Inject(method = "addStatsDisplay", at = @At("HEAD"), remap = false, cancellable = true)
 	private void moretcon$HEAD_Inject$addStatsDisplay(int x, int y, int w, ArrayList<BookElement> list, Material material, LinkedHashSet<ITrait> allTraits, String stattype, CallbackInfo ci) {
-		if (material instanceof UniqueMaterial) {
-			UniqueMaterial umat = (UniqueMaterial) material;
+		if (material instanceof IUniqueMaterial) {
+			IUniqueMaterial umat = (IUniqueMaterial) material;
 			IMaterialStats stats = material.getStats(stattype);
 			if (stats == null) {
 				return;
